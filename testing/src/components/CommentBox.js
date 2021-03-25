137d23d4b2b923904131174a6a669d9e156c32fe
southppp22 <tnddl032@gmail.com>
2021-03-25 22:54:40 +0900

    test reducer

--- testing/src/components/CommentBox.js
+++ testing/src/components/CommentBox.js
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import * as actions from "actions";
 
 const CommentBox = () => {
   const [comment, setComment] = useState("");
+  const dispatch = useDispatch();
 
   const handleChange = (e) => {
     setComment(e.target.value);
